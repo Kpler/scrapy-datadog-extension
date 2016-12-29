45c3a7f40e2947dbab17cdced80743c4573705e9
cmorent <cmorent@dailymotion.com>
2016-12-29 14:36:37 +0100

    Update requirements and fixe code to send metrics when spider is closed

--- scrapydatadog/extension.py
+++ scrapydatadog/extension.py
@@ -13,6 +13,19 @@ class DatadogExtension(object):
         self.dd_api_key = dd_api_key
         self.dd_api_key = dd_api_key
 
+    @classmethod
+    def from_crawler(cls, crawler):
+        sh_api_key = crawler.settings.get('SH_API_KEY')
+        dd_api_key = crawler.settings.get('DATADOG_API_KEY')
+        dd_app_key = crawler.settings.get('DATADOG_APP_KEY')
+        if not sh_api_key or not dd_api_key or not dd_app_key:
+            raise NotConfigured
+        ext = cls(sh_api_key, dd_api_key, dd_app_key)
+        crawler.signals.connect(ext.spider_closed,
+                                signal=signals.spider_closed)
+        return ext
+
+    def spider_closed(self, spider):
         project_id = os.environ.get('SCRAPY_PROJECT_ID')
         spider_id = os.environ.get('SCRAPY_SPIDER_ID')
         job_id = os.environ.get('SCRAPY_JOB_ID')
@@ -41,13 +54,3 @@ class DatadogExtension(object):
                                     tags=["project:{}".format(project_id),
                                           "spider:{}".format(spider_id),
                                           "job:{}".format(job_id)])
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        sh_api_key = crawler.settings.get('SH_API_KEY')
-        dd_api_key = crawler.settings.get('DATADOG_API_KEY')
-        dd_app_key = crawler.settings.get('DATADOG_APP_KEY')
-        if not sh_api_key or not dd_api_key or not dd_app_key:
-            raise NotConfigured
-        ext = cls(sh_api_key, dd_api_key, dd_app_key)
-        return ext
