b099491136fd7822aafe8ce8621a5d5e6c021020
Sébastien Douche <seb@nmeos.net>
2020-01-05 15:39:20 +0100

    Clean setup.py

--- scrapydatadog/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Kpler Engineering',
-__title__ = 'scrapydatadog'
-__version__ = '0.2.7'
--- setup.py
+++ setup.py
@@ -1,37 +1,38 @@
-# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
 
-from __future__ import absolute_import, unicode_literals
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils import setup
-
-import scrapydatadog
-
-packages = [
-    'scrapydatadog',
-]
-
-requires = [
-    'scrapy',
-    'datadog',
-]
 
 setup(
-    name=scrapydatadog.__title__,
-    version=scrapydatadog.__version__,
+    name='scrapy-datadog',
     description='Scrapy extension to send scrapy stats to Datadog.',
-    author=scrapydatadog.__author__,
-    author_email='dev@kpler.com',
+    author='Kpler engineering',
+    author_email='engineering@kpler.com',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(),
     url='http://github.com/kpler/scrapy-datadog-extension',
-    packages=packages,
-    install_requires=requires,
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-    ),
+    ],
+    install_requires = [
+        'scrapy',
+        'datadog',
+    ],
+    extras_require={
+        "test": [
+            'coverage==4.5',
+            'flake8==3.5.0',
+            'ipdb==0.10.3',
+            'mock==2.0.0',
+            'nose==1.3.7',
+            'nose-cov==1.6',
+            'nose-timer==0.7.1',
+            'tox==3.0.0',
+            'tox-pyenv==1.1.0'
+        ]
+    }
 )
